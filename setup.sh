b0748f9e3863347f97889f2aa77ed8d97f147928
Dipanshu Kumar <dipanshu247k@gmail.com>
2025-12-30 14:38:50 +0000

    Updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
 git clone --depth=1  https://github.com/cfig/Android_boot_image_editor image-editor
-echo "Save Dump in folder dump"
-mkdir {build,tmp,dump}
-mkdir -p build/{vendor,system,dtb,ramdisk}
